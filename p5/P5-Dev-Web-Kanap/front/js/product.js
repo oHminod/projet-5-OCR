334005f62a7fdff59ca4db6bf007a4755cb121e0
oHminod <yvesm13@gmail.com>
2022-08-31 16:30:36 +0200

    construction localStorage

--- p5/P5-Dev-Web-Kanap/front/js/product.js
+++ p5/P5-Dev-Web-Kanap/front/js/product.js
@@ -6,6 +6,7 @@ search_params.has('id') ? id = search_params.get('id') : alert("Pas d'id produit
 
 let fin = window.location.href.indexOf("html/") + 5;
 var racine = window.location.href.slice(0, fin) + "index.html";
+var racineP = window.location.href.slice(0, fin) + "product.html";
 
 canap();
 
@@ -44,3 +45,48 @@ function afficherCanap(obj) {
         colors.appendChild(option);
     }
 }
+
+function ajoutPanier(event) {
+    let color = colors.options[colors.selectedIndex].text;
+    if (color === "--SVP, choisissez une couleur --") {
+        alert("Sélectionnez une couleur dans la liste");
+        return
+    }
+    let quantity = parseInt(document.getElementById('quantity').value);
+    if (quantity <= 0) {
+        alert("Sélectionnez une quantité");
+        return
+    }
+    let panier = [id, color, quantity];
+    panier = panier.join('|');
+
+
+    if (localStorage.getItem('panier')) {
+        let tabCommande = localStorage.getItem('panier').split('|');
+        if (tabCommande[0] == id) {
+            if (tabCommande[1] == color) {
+                tabCommande[2] = parseInt(tabCommande[2]);
+                console.log(tabCommande[2]);
+                tabCommande[2] += quantity;
+                localStorage.setItem('panier', tabCommande.join('|'));
+                return
+            }
+        }
+        let commande = [];
+        commande.push(localStorage.getItem('panier'));
+        commande.push(panier);
+        commande = commande.join('%');
+        localStorage.setItem('panier', commande);
+        //alert(tabCommande);
+        return
+        // alert('panier présent');
+        // return
+    }
+    localStorage.setItem('panier', panier);
+    //alert(panier);
+}
+
+document.getElementById('addToCart').addEventListener('click', (e) => {
+    e.preventDefault;
+    ajoutPanier(e);
+})
