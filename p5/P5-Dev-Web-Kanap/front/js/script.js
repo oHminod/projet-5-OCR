b5f4b94d103d4ff78d0e46e43f39e235f15567d7
oHminod <yvesm13@gmail.com>
2022-09-04 00:29:55 +0200

    commentaire script.js + boucle forof au lieu de forin

--- p5/P5-Dev-Web-Kanap/front/js/script.js
+++ p5/P5-Dev-Web-Kanap/front/js/script.js
@@ -1,3 +1,9 @@
+/**
+ * * canaps
+ * Fonction asynchrone renvoyant 
+ * le json du catalogue de produits
+ * ou une erreur.
+ */
 async function canaps() {
     try {
         const reponse = await fetch("http://localhost:3000/api/products");
@@ -9,14 +15,23 @@ async function canaps() {
 }
 
 canaps();
-
+/**
+ * * erreurChargement
+ * Fonction affichant l'erreur de la 
+ * fonction canaps si elle existe
+ * @param  {} erreur
+ */
 function erreurChargement(erreur) {
     const err = document.createElement("h2");
     err.innerText = "Il y a eu un problème lors du chargement du catalogue !";
     items.appendChild(err);
     dump(erreur);
 }
-
+/**
+ * * dump
+ * Fonction pour mettre en forme un message d'erreur
+ * @param  {} obj
+ */
 function dump(obj) {
     var out = '';
     for (var i in obj) {
@@ -28,36 +43,45 @@ function dump(obj) {
     items.appendChild(pre)
 }
 
-
+/**
+ * * afficherCanaps
+ * Fonction d'affichage du résultat de la requête 
+ * effectuée dans la fonction canaps
+ * @param  {} tabObj : tableau d'objets json en format string
+ */
 function afficherCanaps(tabObj) {
     AfficherQuantitePanier();
-    for (let i in tabObj) {
+    for (let item of tabObj) {
         const canap = document.createElement("a");
-        canap.href = `./product.html?id=` + tabObj[i]._id;
+        canap.href = `./product.html?id=` + item._id;
 
         const article = document.createElement("article");
         canap.appendChild(article);
 
         const image = document.createElement("img");
-        image.src = tabObj[i].imageUrl;
-        image.alt = tabObj[i].altTxt;
+        image.src = item.imageUrl;
+        image.alt = item.altTxt;
         article.appendChild(image);
 
         const titre = document.createElement("h3");
         titre.classList.add("productName");
-        titre.innerText = tabObj[i].name;
+        titre.innerText = item.name;
         article.appendChild(titre);
 
         const description = document.createElement("p");
         description.classList.add("productDescription");
-        description.innerText = tabObj[i].description;
+        description.innerText = item.description;
         article.appendChild(description);
 
 
         items.appendChild(canap);
     }
 }
-
+/**
+ * * AfficherQuantitePanier
+ * Fonction qui affiche le nombre d'articles du panier
+ * au niveau du lien vers le panier dans la barre de navigation
+ */
 function AfficherQuantitePanier() {
     if (localStorage.getItem('quantite') && parseInt(localStorage.getItem('quantite')) > 0) {
         let quantite = parseInt(localStorage.getItem('quantite'));
