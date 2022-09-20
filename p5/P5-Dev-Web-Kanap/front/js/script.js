010719345c707646cad6f6d9b2281bdae6360ee5
oHminod <yvesm13@gmail.com>
2022-09-20 21:26:49 +0200

    amélioration commentaires et noms de variable

--- p5/P5-Dev-Web-Kanap/front/js/script.js
+++ p5/P5-Dev-Web-Kanap/front/js/script.js
@@ -1,15 +1,15 @@
 "use strict";
 /**
  * * trouverCatalogue
- * Fonction asynchrone appelant 
+ * Fonction asynchrone appelant
  * une fonction avec le json du catalogue
- * une fonction avec l'erreur.
+ * ou une fonction avec l'erreur.
  */
 async function trouverCatalogue() {
     try {
         const reponse = await fetch("http://localhost:3000/api/products");
-        if (reponse.status >= 400 && reponse.status < 600 || !reponse.ok) {
-            throw new Error(reponse.status + '\n' + reponse.statusText);
+        if ((reponse.status >= 400 && reponse.status < 600) || !reponse.ok) {
+            throw new Error(reponse.status + "\n" + reponse.statusText);
         }
         const resultat = await reponse.json();
         afficherCanaps(resultat);
@@ -20,56 +20,51 @@ async function trouverCatalogue() {
 
 trouverCatalogue();
 
-
-
 /**
  * * erreurChargement
- * Fonction affichant l'erreur de la 
- * fonction canaps si elle existe
+ * Fonction affichant l'erreur de la
+ * fonction trouverCatalogue si elle existe
  * @param  {json} erreur
  */
 function erreurChargement(erreur) {
-    alert(`Il y a eu un problème lors du chargement du catalogue, contacter un administrateur :\n\n${erreur}`);
+    alert(
+        `Il y a eu un problème lors du chargement du catalogue, contacter un administrateur :\n\n${erreur}`
+    );
 }
-
-
 
 /**
  * * afficherCanaps
- * Fonction d'affichage du résultat de la requête 
- * effectuée dans la fonction canaps
- * @param  {json} tabObj : tableau d'objets json
+ * Fonction d'affichage du résultat de la requête
+ * effectuée dans la fonction trouverCatalogue
+ * @param  {json} catalogue : tableau d'objets json
  */
-function afficherCanaps(tabObj) {
+function afficherCanaps(catalogue) {
     AfficherQuantitePanier();
-    for (let item of tabObj) {
+    for (let canape of catalogue) {
         const canap = document.createElement("a");
-        canap.href = `./product.html?id=` + item._id;
+        canap.href = `./product.html?id=` + canape._id;
 
         const article = document.createElement("article");
         canap.appendChild(article);
 
         const image = document.createElement("img");
-        image.src = item.imageUrl;
-        image.alt = item.altTxt;
+        image.src = canape.imageUrl;
+        image.alt = canape.altTxt;
         article.appendChild(image);
 
         const titre = document.createElement("h3");
         titre.classList.add("productName");
-        titre.innerText = item.name;
+        titre.innerText = canape.name;
         article.appendChild(titre);
 
         const description = document.createElement("p");
         description.classList.add("productDescription");
-        description.innerText = item.description;
+        description.innerText = canape.description;
         article.appendChild(description);
-
 
         items.appendChild(canap);
     }
 }
-
-
 
 /**
  * * AfficherQuantitePanier
@@ -77,9 +72,12 @@ function afficherCanaps(tabObj) {
  * au niveau du lien vers le panier dans la barre de navigation
  */
 function AfficherQuantitePanier() {
-    if (localStorage.getItem('quantite') && parseInt(localStorage.getItem('quantite')) > 0) {
-        let quantite = parseInt(localStorage.getItem('quantite'));
-        let panNav = document.querySelector('nav > ul > a:last-child > li');
+    if (
+        localStorage.getItem("quantite") &&
+        parseInt(localStorage.getItem("quantite")) > 0
+    ) {
+        let quantite = parseInt(localStorage.getItem("quantite"));
+        let panNav = document.querySelector("nav > ul > a:last-child > li");
         panNav.innerText = `Panier (${quantite})`;
     }
 }
